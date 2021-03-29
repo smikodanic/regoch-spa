ae290cbcfa81f20ecde586634b89a501cd459574
Sasa M <smikodanic@gmail.com>
2021-03-29 19:43:22 +0200

    loadInc test

--- app/src/controllers/playground/LoadIncTestCtrl.js
+++ app/src/controllers/playground/LoadIncTestCtrl.js
@@ -9,22 +9,28 @@ class LoadIncTestCtrl extends Controller {
 
 
   async prerender(trx) {
-    this.setTitle('Page Test');
+    this.setTitle('loadInc() Test');
     this.setDescription('Page Test description');
     this.setKeywords('regoch, playground, test, page');
     this.setLang('en');
+
+    this.loadCSS(['https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism-coy.min.css']);
 
     await this.loadView('#primary', 'playground/loadIncTest/primary.html', 'sibling'); // inner, outer, sibling, prepend, append
   }
 
 
   async postrender(trx) {
+    this.lazyJS([
+      'https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/prism.min.js'
+    ]);
 
   }
 
 
   destroy(elem, event) {
-
+    this.unlazyJS();
+    this.unloadCSS(['https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism-coy.min.css']);
   }
 
 
