004dd1d48427a248d848a9ad41963e54be6d10b3
Sasa M <smikodanic@gmail.com>
2021-04-03 11:02:53 +0200

    bin init

--- bin/init.js
+++ bin/init.js
@@ -2,29 +2,46 @@ const path = require('path');
 const fse = require('fs-extra');
 
 
-module.exports = async (projectName) => {
+module.exports = async (appName) => {
 
   try {
-    // copy task_template folder projectName folder
-    await fse.ensureDir(projectName);
-    const sourceDir = path.join(__dirname, './task_template');
-    const destDir = path.join(process.cwd(), projectName);
-    await fse.copy(sourceDir, destDir);
-    console.log(`Copied from ${sourceDir} to ${destDir}`);
+    // destination directory
+    const destDir = path.join(process.cwd(), appName);
+    await fse.ensureDir(destDir);
+
+    // copy files from the /template/ folder to /appName/ folder
+    const sourceTemplate = path.join(__dirname, 'template');
+    await fse.copy(sourceTemplate, destDir);
 
     // rename gitignore (npm does not publish task_template/.gitignore so task_template/gitignore is used)
     const gitignore_old = path.join(destDir, 'gitignore');
     const gitignore_new = path.join(destDir, '.gitignore');
     await fse.rename(gitignore_old, gitignore_new);
 
-    const tf = await fse.pathExists(destDir);
-    if (tf) {
-      console.log(`Task "${projectName}" initialized and folder is created.`);
+    // copy .gulp folder
+    const sourceGulp = path.join(__dirname, '../.gulp');
+    const destGulp = path.join(destDir, '.gulp');
+    await fse.copy(sourceGulp, destGulp);
+
+    // copy server folder
+    const sourceServer = path.join(__dirname, '../server');
+    const destServer = path.join(destDir, 'server');
+    await fse.copy(sourceServer, destServer);
+
+    // copy other files
+    const files = ['.editorconfig', '.eslintrc', 'gulpfile,js', 'LICENSE'];
+    for (const file of files) {
+      const sourceFile = path.join(__dirname, '../.editorconfig');
+      const destFile = path.join(destDir, file);
+      await fse.copy(sourceFile, destFile);
     }
 
+    // ending message
+    const tf = await fse.pathExists(destDir);
+    if (tf) { console.log(`Task "${appName}" initialized and folder is created.`); }
 
   } catch (err) {
-    throw err;
+    console.error(err);
   }
 
 };
---
+++ bin/template/README.md
@@ -0,0 +1,12 @@
+# regoch-spa
+> Regoch Single Page Application framework.
+
+
+## Documentation
+[http://www.regoch.org/single-page-app](http://www.regoch.org/single-page-app)
+
+
+### Licence
+“Freely you received, freely you give”, Matthew 10:5-8
+
+Copyright (c) 2020 Saša Mikodanić licensed under [AGPL-3.0](./LICENSE) .
